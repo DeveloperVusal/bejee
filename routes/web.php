fc7ad516b04c2dce33ad912dc157c6f1c758cd25
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-18 17:00:49 +0300

    test: small fix for tests

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
+use Core\Facades\Database\MariaDB;
+use Core\Facades\Render\View;
+
 $query = $_SERVER['REQUEST_URI'];
 
 switch ($query) {
+    // case '/db':
+    //     $db = MariaDB::connection();
+    //     break;
+
     case '/':
-        include __DIR__.'./../web/todo/index.htm';
+        // include __DIR__.'./../web/todo/index.htm';
+
+        $v = View::get('todo', 'index.htm.php');
+
         break;
 
     case '/tasks':
