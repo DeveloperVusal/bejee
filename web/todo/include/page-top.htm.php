d0cebf6b0c34a6f6d74ebf9a8b2a501ee9a37f0c
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-19 15:54:37 +0300

    style: add custom style for status task

--- web/todo/include/page-top.htm.php
+++ web/todo/include/page-top.htm.php
@@ -6,5 +6,7 @@
     <title>Todo List</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="./web/todo/css/app.css">
 </head>
 <body>
--- web/todo/index.htm.php
+++ web/todo/index.htm.php
@@ -33,13 +33,17 @@
         </div>
     </div>
     <div class="row mt-3">
-        <div class="card mb-3">
+        <div class="card mb-3 position-relative">
             <div class="card-body">
                 <h5 class="card-title">Задача #0001</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Vusal (vusal@mail.ru)</h6>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="card-link">Редактировать</a>
                 <a href="#" class="card-link text-danger">Удаить</a>
+
+                <div class="position-absolute top-0 end-0 bg-success text-white card-status">
+                    Done
+                </div>
             </div>
         </div>
         <div class="card mb-3">
