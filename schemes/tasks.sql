75e26afe95635136d7ea7b5bf31d8f38add912f9
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-19 21:35:06 +0300

    style: rename column of tasks table

--- schemes/tasks.sql
+++ schemes/tasks.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `tasks` (
     `id` BIGINT AUTO_INCREMENT,
-    `edit_user_id` BIGINT,
+    `save_user_id` BIGINT,
     `name` VARCHAR(64) NOT NULL,
     `email` VARCHAR(64) NOT NULL,
     `text` TINYTEXT NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `tasks` (
     `updated_at` DATETIME DEFAULT NULL,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     PRIMARY KEY(`id`),
-    FOREIGN KEY(`edit_user_id`) REFERENCES `users`(`id`),
+    FOREIGN KEY(`save_user_id`) REFERENCES `users`(`id`),
     INDEX (`name`),
     INDEX (`email`)
 );
