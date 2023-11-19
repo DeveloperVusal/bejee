85e4863b88665ae605ab3ec96cdf48dcc2f89f82
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-19 17:44:11 +0300

    feat: add the Response handler for work with requests

---
+++ core/http/Response.php
@@ -0,0 +1,60 @@
+<?php
+namespace Core\Http;
+
+enum Status
+{
+    case Error;
+    case Warning;
+    case Success;
+}
+
+final class Response {
+
+    /**
+     * @param Status $status
+     * @param int $code
+     * @param string $message
+     * @param array $result
+     */
+    function __construct(
+        private Status $status = Status::Success,
+        private int $code,
+        private string $message,
+        private array $result,
+    ) {}
+    
+    /**
+     * The method builds of the properties to array
+     * 
+     * @return array
+     */
+    public function build(): array
+    {
+        return [
+            'status' => $this->status,
+            'code' => $this->code,
+            'message' => $this->message,
+            'result' => $this->result,
+        ];
+    }
+
+    /**
+     * The method create the response like json format
+     * 
+     * @return string
+     */
+    public function create(): string
+    {
+        return json_encode($this->build());
+    }
+    
+    /**
+     * This method print to screen result of response
+     * 
+     * @return void
+     */
+    public function echo(): void
+    {
+        echo $this->create();
+    }
+}
