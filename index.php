3d1785e17c492f95812d2f5ecfc4bc29c1d5958a
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-19 18:00:07 +0300

    fix: add the api routes in index.php

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ require __DIR__.'./vendor/autoload.php';
 require __DIR__.'./autoload/register.php';
 require __DIR__.'./autoload/helpers.php';
 
+include __DIR__.'./routes/api.php';
 include __DIR__.'./routes/web.php';
