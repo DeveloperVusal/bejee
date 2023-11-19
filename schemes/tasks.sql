e6245e99ff3618e3aa65a77ae369bd2c6c517604
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-19 14:24:22 +0300

    chore: add foreign key

--- schemes/tasks.sql
+++ schemes/tasks.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS `tasks` (
     `id` BIGINT AUTO_INCREMENT,
+    `edit_user_id` BIGINT,
     `name` VARCHAR(64) NOT NULL,
     `email` VARCHAR(64) NOT NULL,
     `text` TINYTEXT NOT NULL,
     `updated_at` DATETIME DEFAULT NULL,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     PRIMARY KEY(`id`),
+    FOREIGN KEY(`edit_user_id`) REFERENCES `users`(`id`),
     INDEX (`name`),
     INDEX (`email`)
 );
