7a2411575d9a2eebc3fe52f9d4038a6956f5bd35
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-20 03:08:46 +0300

    fix: enable sessions

--- index.php
+++ index.php
@@ -4,5 +4,7 @@ require __DIR__.'./vendor/autoload.php';
 require __DIR__.'./autoload/register.php';
 require __DIR__.'./autoload/helpers.php';
 
+session_start();
+
 include __DIR__.'./routes/api.php';
 include __DIR__.'./routes/web.php';
